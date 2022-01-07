9b204d1e543b98cf8ab0daed33ec9fa6fb5c26f8
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-01-07 00:52:02 -0500

    Added random character variables and prompted user with window.confirms of which characters they'd want.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '_'];
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
  
 
 
@@ -19,8 +23,28 @@ if (passLength != passLength) {
   return "Error, NaN";
 }
 
-if ((passLength < 8) || (passLength > 128))
+if ((passLength < 8) || (passLength > 128)) {
   return "Must choose a valid number";
+}
+
+// USER MUST CONFRIM IF THEY WANT CAPS, LOWERCASE, NUMBERS OR SPECIAL CHARACTERS
+ var charChosen = [];
+if (window.confirm("Do you want to include special characters?")){
+charChosen = charChosen.concat(specialChar);
+}
+if (window.confirm("Do you want to include lowercase letters?")) {
+  charChosen = charChosen.concat(lowerCaseChar);
+}
+if (window.confirm("Do you want to include uppercase letters?")) {
+  charChosen = charChosen.concat(upperCaseChar);
+}
+if (window.confirm("Do you want to include numbers?")){
+  charChosen = charChosen.concat(numChar)
+}
+
+// PASSWORD MUST NOW BE ABLE TO PICK RANDOM CHARACTERS FROM ARRAY 
+
+var password = "";
 
 
 
@@ -46,9 +70,7 @@ if ((passLength < 8) || (passLength > 128))
 
 
 
-
-
-
+return password;
    
  }
 // Write password to the #password input
